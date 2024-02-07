a07ae07458feaba625812ba421961a6b066ed32f
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-07 17:16:28 +0530

    Video 57 -  Js Loops

---
+++ Video 57/index.js
@@ -0,0 +1,45 @@
+console.log("I am a tutorial on Loops")
+
+// let a =1;
+
+// for (let i = 0; i < 10; i++) {
+    
+//     console.log(a+i);
+// }
+
+// let women = {
+//     name: "Megh",
+//     role: "Programmer",
+//     age: "18",
+//     net_worth: "0"
+// }
+
+
+// // for key or element - for in
+// for (const key in women) {
+//         const element = women[key]; 
+//         console.log(key, element)
+// }
+
+// // for itrator - for in
+// for (const c of "Megh") {
+//     console.log(c);
+// }
+
+
+// Now While Loop
+// let i = 1;
+
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+// Do while Loop
+
+let i = 1;
+
+do {
+    console.log(i);
+    i++
+} while (i<6);
