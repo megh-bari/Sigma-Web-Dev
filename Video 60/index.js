f0b12137f8739de4235df64b817750b1bd6a27ed
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-07 17:17:48 +0530

    Video 60 - Js Strings

---
+++ Video 60/index.js
@@ -0,0 +1,30 @@
+console.log("This is Strings Tutorial")
+
+let a = "Megh";
+// Index start from Zero
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+// console.log(a[4])
+
+console.log(a.length)
+
+let real_name = "Megh"
+let friend = "IDK"
+
+console.log("His name is " + real_name + " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his freind name is ${friend}`) 
+
+let b = "Nothing"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,6))
+
+console.log(b.replace("thing", "18"))
+console.log(b.concat(a," and Sam"))
+console.log(b.charAt(1))
+console.log(b.endsWith(0))
+console.log(b.startsWith("N"))
+
