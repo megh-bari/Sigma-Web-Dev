d6bdb6ba9d8fa33a5f7e50cfbfe3f0c3f5fa2b8e
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-08 21:14:31 +0530

    Video 67 -  Js DOM, Children, Parent & Sibling Nodes

---
+++ Video 67/index.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+
+let cont = document.body.childNodes[1];
+
+cont.lastChild.style.color = "red";
+
+cont.lastElementChild.style.color = "red";
+
+cont.lastElementChild.style.backgroundColor = "yellow";
+
+cont.lastElementChild.parentElement;
+
+
+
+document.body.firstElementChild;
+
+document.body.firstElementChild.childNodes;
+
+document.body.firstElementChild.children;
+
+document.body.firstElementChild.children[3];
+
+document.body.firstElementChild.children[3].nextElementSibling;
+
+document.body.firstElementChild.children[3].previousElementSibling;
+
+
+
+document.body.children;
+
+document.body.children[1];
+
+document.body.children[1].rows;
