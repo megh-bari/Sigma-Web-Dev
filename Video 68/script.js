5c7eb918dc5a9602853413894890304afd34ae61
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-08 21:14:54 +0530

    Video 68 -  Js Selecting by Ids, Classes and More

---
+++ Video 68/script.js
@@ -0,0 +1,26 @@
+console.log("Megh")
+
+// Using Classname
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "yellow"
+
+
+// Using Id
+// document.getElementById("box-3").style.backgroundColor = "yellow"
+
+// Usind Query Selector
+// document.querySelector(".box").style.backgroundColor = "green"
+
+// Usinf Query Selector All
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e=>{
+    console.log(e.style.backgroundColor = "yellow")
+})
+
+
+document.getElementsByTagName("div")
+
+
