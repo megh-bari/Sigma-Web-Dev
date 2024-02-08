e43a0d4f3aa7c2551f9fbc69f27fcc77f18805f8
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-08 21:13:56 +0530

    Video 66 - DOM In Js

---
+++ Video 66/index.js
@@ -0,0 +1,12 @@
+// DOM In Js - Document Object Model
+
+document.title = "Megh Bro"
+console.log(document.title)
+console.log(document.body)
+document.body.style.backgroundColor = "yellow"
+
+
+// BOM In JS - Browser Object Model
+
+
+
