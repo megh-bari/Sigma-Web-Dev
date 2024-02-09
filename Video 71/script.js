7a3cda1337c4235304d1925611f27066cafa113d
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-09 22:15:56 +0530

    Video 71 - Inserting and Removing Element in Js

---
+++ Video 71/script.js
@@ -0,0 +1,69 @@
+document.querySelector(".box").innerHTML
+
+document.querySelector(".box").innerText
+
+document.querySelector(".container").innerHTML
+
+document.querySelector(".container").innerText
+
+document.querySelector(".container").outerHTML
+
+document.querySelector(".container").tagName
+
+document.querySelector(".container").nodeName
+
+document.querySelector(".container").textContent
+
+document.querySelector(".container").hidden 
+
+// document.querySelector(".container").hidden = true
+
+// document.querySelector(".container").innerHTML ="Hey I'm Megh"
+
+document.querySelector(".box").hasAttribute("style")
+
+document.querySelector(".box").getAttribute("style")
+
+document.querySelector(".box").setAttribute("style","display: inline")
+
+// document.querySelector(".box").removeAttribute("style")
+
+// document.designMode = "on"
+
+// document.querySelector(".box").dataset
+
+
+
+// let div = document.createElement("div");
+// div.innerHTML = "Hey I am Div Created by JS";
+// div.setAttribute("class", "created");
+// document.body.querySelector(".container").append(div);
+// document.body.querySelector(".container").before(div);
+// document.body.querySelector(".container").prepend(div);
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("afterbegin", "<b> Hey, I am under the water. Please help me here too much raining... me</b>")
+// cont.insertAdjacentHTML("afterend", "<b> Hey, I am under the water. Please help me here too much raining...</b>")
+// cont.insertAdjacentHTML("beforebegin", "<b> Hey, I am under the water. Please help me here too much raining...</b>")
+// cont.insertAdjacentHTML("beforeend", "<b> Hey, I am under the water. Please help me here too much raining...</b>")
+
+
+document.querySelector(".box").remove(1)
+
+document.querySelector(".container").classList
+
+document.querySelector(".container").className
+
+document.querySelector(".container").classList.add("Megh")
+
+document.querySelector(".container").classList.remove("red")
+
+
+
+
+
+
+
+
+
+
