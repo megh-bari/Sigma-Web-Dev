b3e1fce30705ad7d29262e4086c29e7ed9a7d8da
Megh Bari <142393952+megh-bari@users.noreply.github.com>
2024-02-11 17:28:39 +0530

    Video 74 -  Events. Event Bubblimg, setInterval & setTimeout in Js

---
+++ Video 74/script.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("btn")
+
+// button.addEventListener("click", ()=>{
+//     document.querySelector(".box").innerHTML = "<b>Hey You clicked this button</b>"
+// })
+
+
+// button.addEventListener("dblclick", ()=>{
+//     document.querySelector(".box").innerHTML = "<b>Hey You clicked this button</b>"
+// })
+
+// button.addEventListener("contextmenu", ()=>{
+//    alert("Don't right click ")
+// })
+
+document.addEventListener("keydown", (e)=>{
+   console.log(e.key, e.keyCode)
+ })
+ 
+ 
